db609d6bc78db548df26a5abe9385e2a6ff17627
mel <gilsebastian019@gmail.com>
2024-06-06 14:52:57 -0500

    Se emplean funciones y metodos para tachar el TODO al completar y/o eliminar los elementos haciendo la aplicación más viva reaccionando a lo que el usuario haga en ella

--- src/App.js
+++ src/App.js
@@ -1,52 +1,76 @@
-import React from 'react';
-import { TodoCounter } from './TodoCounter/TodoCounter';
-import { TodoSearch } from './TodoSearch/TodoSearch';
-import { TodoList } from './TodoList/TodoList';
-import { TodoItem } from './TodoItem/TodoItem';
-import { CreateTodoButton } from './TodoButton/CreateTodoButton';
+import React from "react";
+import { TodoCounter } from "./TodoCounter/TodoCounter";
+import { TodoSearch } from "./TodoSearch/TodoSearch";
+import { TodoList } from "./TodoList/TodoList";
+import { TodoItem } from "./TodoItem/TodoItem";
+import { CreateTodoButton } from "./TodoButton/CreateTodoButton";
 
 const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro de react', completed: false },
-  { text: 'Subir a diamante en valorant', completed: false },
-  { text: 'Dormir', completed: false }
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el curso de intro de react", completed: false },
+  { text: "Subir a diamante en valorant", completed: false },
+  { text: "Dormir", completed: false },
 ];
 
 function App() {
   //Estados
   const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
 
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
   //La doble negación es para saber si son valores booleanos.
   const totalTodos = todos.length;
 
-  console.log('Usuarios buscan TODOS de ' + searchValue);
+  const searchedTodos = todos.filter((todo) => {
+    // Hay dos maneras de llamar el estado con su metodo toLowerCase
+    // 1
+    // return todo.text.toLowerCase().includes(searchValue.toLocaleLowerCase());
+    // 2
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+  );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+  
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    // El splice nos pide una posición del array y nos pide cuantos elementos debo quitar
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
   return (
     // <react.fragment>
     <>
- {/* Insertar/retornar un componente de react dentro de otro componente de react */}
-      <TodoCounter 
-      completed={completedTodos} 
-      total={totalTodos}
-      />
-      <TodoSearch 
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      />
-     
+      {/* Insertar/retornar un componente de react dentro de otro componente de react */}
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+
       <TodoList>
-        {defaultTodos.map(todo =>(
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text}
-          completed={todo.completed}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
 
       <CreateTodoButton />
-      </>
+    </>
   );
 }
 
